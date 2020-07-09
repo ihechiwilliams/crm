7be2cb40fd1b9e0b3750c78c49eb92836e6521bb
Williams Charles <53452272+charleyvibez@users.noreply.github.com>
2020-07-09 07:44:02 +0100

    removed my email details

--- CRM/settings.py
+++ CRM/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'charlesclinton2003@gmail.com'
-EMAIL_HOST_PASSWORD = 'henry2014?'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 django_heroku.settings(locals())
